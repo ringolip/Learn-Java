026f5d795405828c3b32358d81383df954784532
ringolip <zhangringo@gmail.com>
2020-04-05 13:23:43 +0800

    Day5 数组

---
+++ Day5/Demo01Array.java
@@ -0,0 +1,15 @@
+package ringo.day05.demo01;
+
+public class Demo01Array {
+    public static void main(String[] args) {
+        // 创建一个数组，可以存放300个int数据
+        // 格式：数据类型[] 数组名称 = new 数据类型[数据个数];
+        int[] arrayA = new int[300];
+
+        // 创建一个数组，可以存放10个double类型数据
+        double[] arrayB = new double[10];
+
+        // 创建一个数组，可以存放5个字符串
+        String[] arrayC = new String[5];
+    }
+}
---
+++ Day5/Demo02Array.java
@@ -0,0 +1,10 @@
+package ringo.day05.demo01;
+
+public class Demo02Array {
+    public static void main(String[] args) {
+        // 静态初始化数组
+        int[] arrayA = new int[]{10, 15, 25};
+        String[] arrayB = new String[]{"Hello", "World", "Java"};
+
+    }
+}
