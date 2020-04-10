b6554469fd7de9263ea093442db5ba86c278a5bb
ringolip <zhangringo@gmail.com>
2020-04-10 19:56:09 +0800

    Day11 final关键字

---
+++ Day11/Dad.java
@@ -0,0 +1,7 @@
+package ringo.day11.demo01;
+
+public class Dad {
+    public final void method() {
+        System.out.println("父类方法执行");
+    }
+}
---
+++ Day11/Son.java
@@ -0,0 +1,7 @@
+package ringo.day11.demo01;
+
+public class Son extends Dad{
+    public void method(){
+        
+    }
+}
